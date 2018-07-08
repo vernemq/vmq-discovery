d0797b8aee64caa670dc4a03859935d3201aa8d8
Dairon Medina Caro <dairon@gooee.com>
2018-07-08 03:07:42 -0400

    Update header

--- src/vmq_discovery_autocluster.erl
+++ src/vmq_discovery_autocluster.erl
@@ -1,4 +1,4 @@
-%% Copyright 2018 Octavo Labs AG (https://vernemq.com/company.html)
+%% Copyright 2018 Octavo Labs AG Zurich Switzerland (https://octavolabs.com)
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
